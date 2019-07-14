239be1dd6e2ee0dff5ecba138cb19ad290252fc3
Alexander Vysotsky <alysottt@gmail.com>
2019-07-15 01:20:21 +0600

    Added tool for work with languages

---
+++ resources/lang/en/content.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'courses' => 'Courses',
+    'about us' => 'About us',
+    'news' => 'News',
+    'contact us' => 'Contact us',
+    'chlang' => 'Choose language',
+    'edcourse' => 'Education Course',
+    'presentation' => 'We are pleased to present for you a new educational course that will allow you to improve your social communication skills and increase your professional level.',
+
+];
---
+++ resources/lang/fr/content.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+
+    'chlang' => 'Choisissez la langue',
+    'edcourse' => 'Cours d`éducation',
+    'presentation' => 'Nous sommes heureux de vous présenter un nouveau cours éducatif qui vous permettra d’améliorer vos compétences en communication sociale et d’améliorer votre niveau professionnel.',
+
+];
