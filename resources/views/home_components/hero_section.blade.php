61e4fd104ea22a7bf16170b91c49de67768993c7
newmanforlife <newmanforlife@list.ru>
2019-07-15 11:19:24 +0600

    sectional separation

---
+++ resources/views/home_components/hero_section.blade.php
@@ -0,0 +1,17 @@
+<section class="hero-section set-bg" data-setbg="https://static.hmq-edu.com/img/bg.jpg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7">
+                <div class="hero-text text-white">
+                    <h2>Special HMQ-Education Course</h2>
+                    <p>We are pleased to present for you a new educational course that will allow you to improve your
+                        social communication skills and increase your professional level.</p>
+                    <div class="hero-author">
+                        <div class="hero-author-pic set-bg" data-setbg="https://static.hmq-edu.com/img/hero-author.jpg"></div>
+                        <h5>By Victoria Arabina, <span>Professional teacher & coach</span></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
