e66385860faacae780dc9fd2b1adf86b83c60cd3
newmanforlife <newmanforlife@list.ru>
2019-07-15 23:46:07 +0600

    add policy file

---
+++ app/Policies/CoursePolicy.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Course;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CoursePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can view any courses.
+     *
+     * @param  \App\User $user
+     * @return mixed
+     */
+    public function viewAny(User $user)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can view the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function view(User $user, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can create courses.
+     *
+     * @param  \App\User $user
+     * @return mixed
+     */
+    public function create(User $user)
+    {
+        // ЕСЛИ ЮЗЕР - ЭТО ПРЕПОД
+    }
+
+    /**
+     * Determine whether the user can edit the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function update(User $user, Course $course)
+    {
+        return $user->id == $course->created_by;
+    }
+
+    /**
+     * Determine whether the user can update the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function update(User $user, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can delete the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function delete(User $user, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can restore the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function restore(User $user, Course $course)
+    {
+        //
+    }
+
+    /**
+     * Determine whether the user can permanently delete the course.
+     *
+     * @param  \App\User $user
+     * @param  \App\Course $course
+     * @return mixed
+     */
+    public function forceDelete(User $user, Course $course)
+    {
+        //
+    }
+}
