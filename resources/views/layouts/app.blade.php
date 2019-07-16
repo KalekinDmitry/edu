5e48be2ffdc08b92c5034820644984defef79fef
jebbyk <zaicev9797@gmail.com>
2019-07-16 23:49:08 +0600

    Some comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,33 +24,35 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
 
-
 <body>
-
     <!-- Page Preloder -->
     <div id="preloder">
         <div class="loader"></div>
     </div>
 
     <div id="app">
+         {{-- header --}}
         @include('includes.header')
-        <main class="py-0">
-            @yield('content')
-        </main>
+         {{-- header --}}
+         <section>
+                <main class="py-0">
+                    @yield('content')
+                </main>
+         </section>
     </div>
-
+     {{-- footer --}}
     @include('includes.footer')
-
+     {{-- footer --}}
     <!--====== Javascripts & Jquery ======-->
-    <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
-    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
-    <script src="{{ asset('js/circle-progress.min.js') }}"></script>
-    <script src="{{ asset('js/main.js') }}"></script>
-
-
+    <section>
+            @yield('scripts')
+            <script src="{{ asset('js/jquery-3.2.1.min.js') }}"></script>
+            <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+            <script src="{{ asset('js/owl.carousel.min.js') }}"></script>
+            <script src="{{ asset('js/circle-progress.min.js') }}"></script>
+            <script src="{{ asset('js/main.js') }}"></script>
+    </section>
 </body>
 </html>
