9e754d78ef435f9764335cf90dfc9c97e0e6f4db
jebbyk <zaicev9797@gmail.com>
2019-07-19 21:14:36 +0600

    lesson resourse controller, lesson resourse policy, lesson model

---
+++ app/Http/Controllers/LessonController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Course;
+use App\Models\Lesson;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LessonController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('auth:teacher')->except('show');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(Course $course)
+    {
+        $teacher = Auth::user();
+        if($teacher->can('create', $course )){
+            return view('lesson.create');
+        }else return redirect()->route('course.show', $course);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Course $course)
+    {
+        $teacher = Auth::user();
+        if($teacher->can('store', $course)){
+            $lesson = Lesson::create($request->all());
+            $lesson->slug = Str::slug($lesson->title);
+            $lesson->course_id = $course->id;
+            $lesson->save();
+            return redirect()->route('lesson.show', $lesson->id);
+        }
+        else return redirect()->route('course.show', $course);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($lesson)
+    {
+        return veiw('lesson.show', ['lesson' => $lesson]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lesson $lesson, Course $course)
+    {
+        $teacher = Auth::user();
+        if($teacher->can('edit', $lesson, $course)){
+            return view('lesson.edit',['lesson' => $lesson] );
+        }else return redirect()->route('course.show', $course);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lesson $lesson, Course $course)
+    {
+        $teacher = Auth::user();
+        if($teacher->can('update', $lesson, $course)){
+            $lesson->update($request->except('slug'));
+            $lesson->save();
+            return redirect()->route('lesson.show', $lesson);
+        }
+        else return redirect()->route('course.show', $lesson);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lesson $lesson, Course $course)
+    {
+        $teacher = Auth::user();
+        if($teacher->can('destroy', $lesson, $course)){
+            $lesson->delete();
+        }
+        return redirect()->route('course.show', $course);
+    }
+}
---
+++ resources/views/lesson/edit.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h0>This is a show lesson view</h0>
+
+@endsection
