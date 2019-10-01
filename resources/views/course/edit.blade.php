55f84003a121cda0ded080bc0d213337b118b995
Elkin Alexandr <newmanforlife@list.ru>
2019-10-01 22:07:29 +0600

    [FIX] Course create and edit improved

--- resources/views/course/edit.blade.php
+++ resources/views/course/edit.blade.php
@@ -1,14 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-
     <!-- Page top section -->
-    <section class="page-top-section set-bg fixed " style="min-height: 200vh" data-setbg="{{config('static.static')}}/img/page-top-bg.jpg">
+    <section class="page-top-section set-bg fixed " style="min-height: 200vh"
+             data-setbg="{{config('static.static')}}/img/page-top-bg.jpg">
         <div class="container text-white">
             <h3>@lang('content.edcour')</h3>
         </div>
         <!--  Page top end -->
-
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card ">
@@ -17,7 +16,8 @@
                             <div class="col-md-5">
                                 <div class="col-md-12">
                                     <!-- Header section end -->
-                                    <a class="site-btn-info-sm col-md-12" href="{{ route('module.create', $course->id) }}">@lang('content.addnm')</a>
+                                    <a class="site-btn-info-sm col-md-12"
+                                       href="{{ route('module.create', $course->id) }}">@lang('content.addnm')</a>
                                 </div>
                                 <br><br><br>
                                 @foreach($modules as $module)
@@ -47,13 +47,14 @@
                                                     <a class="site-btn-hollow col-md-3"
                                                        href="{{route('lesson.edit', [$module->id, $lesson->id])}}"><i
                                                                 class="fa fa-edit"></i></a>
-                                                    <form style="padding: 0" class = "col-md-1"
+                                                    <form style="padding: 0" class="col-md-1"
                                                           onsubmit="if(confirm('Delete module?')){return true}else{return false}"
                                                           action="{{route('lesson.destroy', [$module, $lesson])}}"
                                                           method="post">
                                                         <input type="hidden" name="_method" value="Delete">
                                                         {{ csrf_field() }}
-                                                        <button type="submit" class="site-btn-danger col-md-12">X</button>
+                                                        <button type="submit" class="site-btn-danger col-md-12">X
+                                                        </button>
                                                     </form>
                                                 </div>
                                             @endforeach
@@ -65,34 +66,28 @@
                                     </div>
                                 @endforeach
                             </div>
-
-
-                            <div class="col-md-7">
+                            <div class="col-md-7" style="text-align: left;">
                                 <!-- course -->
                                 <div class="course-item featured">
-                                    <div class="course-content">
-                                        <div class="cc-text">
-                                            <form class="form-horizontal card-body"
-                                                  action="{{route('course.update', $course)}}"
-                                                  method="post"
-                                                  enctype="multipart/form-data">
-                                                <input type="hidden" name="_method" value="put">
-                                                {{ csrf_field() }}
+                                    <form class="form-horizontal card-body"
+                                          action="{{route('course.update', $course)}}"
+                                          method="post"
+                                          enctype="multipart/form-data">
+                                        <input type="hidden" name="_method" value="put">
+                                        {{ csrf_field() }}
 
-                                                {{-- Form include --}}
-                                                @include('course.partials.form')
+                                        {{-- Form include --}}
+                                        @include('course.partials.form')
 
-                                                <div class="form-group">
-                                                    <label for="">@lang('content.chcovifch')</label><br>
+                                        <div class="form-group" style="margin-left:0;">
+                                            <label for="">@lang('content.chcovifch')</label><br>
 
-                                                    <input type="file" name="image">
-                                                </div>
-                                                <input class="site-btn col-md-4" type="submit" value="To apply">
-                                                <a class="site-btn-hollow"
-                                                   href="{{ route('teacher.dashboard') }}">@lang('content.cancel')</a>
-                                            </form>
+                                            <input type="file" name="image">
                                         </div>
-                                    </div>
+                                        <button class="site-btn" type="submit">To apply<i class="fa fa-angle-right"></i></button>
+                                        <a class="site-btn-danger"
+                                           href="{{ route('teacher.dashboard') }}">@lang('content.cancel')</a>
+                                    </form>
                                 </div>
                             </div>
                         </div>
@@ -100,9 +95,6 @@
                 </div>
             </div>
         </div>
-
-
     </section>
     <!-- Courses section end -->
-
 @endsection
