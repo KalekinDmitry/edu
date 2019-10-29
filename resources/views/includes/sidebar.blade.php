6d60ef60cf870d57b182d4aec0af39026a5fbb66
jebbyk <zaicev9797@gmail.com>
2019-10-30 01:26:43 +0600

    [FRONT] improve sidebar for teacher

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,7 +1,7 @@
 
 @if(!Request::is('user/login') && !Request::is('teacher/login') && !Request::is('user/register') && !Request::is('teacher/register'))
 <div class="sidebar bg-dark" id="sidebar">
-  <div class="py-4 px-3   pb-0 mb-0 bg-dark">
+    <div class="py-4 px-3   pb-0 mb-0 bg-dark">
         @if (Request::is('/')|| Request::is('course/*'))
             @include('sidebar_components.login_register_section')
         @endif
@@ -10,76 +10,18 @@
             @include('sidebar_components.user_section')
         @endif
 
-  </div>
+    </div>
 
-  <hr class="pt-0 mt-0" style="background-color:white; opacity: 0.1">
+    <hr class="pt-0 mt-0" style="background-color:white; opacity: 0.1">
 
-  <ul class="nav flex-column bg-dark mb-0">
+    <ul class="nav flex-column bg-dark mb-0">
+        @if(Request::is('teacher/*') || Request::is('teacher'))
+            @include('sidebar_components.nav_section_teacher')
+        @endif
 
-    <li class="nav-item  m-1 ml-2">
-      <a href="{{ route('home') }}" class="nav-link text-white font-weight-normal ">
-        <img src="{{ asset('assets/icons/icon_home.svg') }}"  width="16px">
-        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-        Новости
-      </a>
-    </li>
-
-    <li class="nav-item  m-1 ml-2">
-        <a href="#" class="nav-link text-white font-weight-normal " >
-            <img src="{{ asset('assets/icons/icon_courses.svg') }}"  width="16px">
-            <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-            Курсы
-        </a>
-    </li>
-
-    <li class="nav-item  m-1 ml-2">
-        <a href="#" class="nav-link text-white font-weight-normal; ">
-            <img src="{{ asset('assets/icons/icon_calendar.svg') }}"  width="16px">
-            <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-            Календарь
-        </a>
-    </li>
-
-    @guest
-
-    @else
-        @if(Request::is('user'))
-            <li class="nav-item m-1 ml-2">
-                <button class="btn btn-primary text-white font-weight-normal" >
-                    <div class="row justify-content-left">
-                        <img src="{{ asset('assets/icons/icon_messages.svg') }}" width="16px">
-                        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-                        Сообщения
-                    </div>
-                </button>
-            </li>
-            @else
-            <li class="nav-item  m-1 ml-2">
-                <a href="{{ route('user.dashboard')}}" class="nav-link text-white font-weight-normal; " >
-                    <img src="{{ asset('assets/icons/icon_messages.svg') }}"  width="16px">
-                    <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-                    Сообщения
-                </a>
-            </li>
-            @endif
-    @endguest
-
-
-    <li class="nav-item  m-1 ml-2">
-        <a href="#" class="nav-link text-white font-weight-normal " >
-            <img src="{{ asset('assets/icons/icon_eduresrc.svg') }}"  width="16px">
-            <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-            Обучающие ресурсы
-        </a>
-    </li>
-
-    <li class="nav-item m-1 ml-2">
-        <a href="#" class="nav-link text-white font-weight-normal " >
-            <img src="{{ asset('assets/icons/icon_help.svg') }}"  width="16px">
-            <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
-            Помощь
-        </a>
-    </li>
+        @if(Request::is('user') || Request::is('user/*'))
+            @include('sidebar_components.nav_section_student')
+        @endif
     </ul>
     {{-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>--}}
---
+++ resources/views/sidebar_components/nav_section_teacher.blade.php
@@ -0,0 +1,77 @@
+@if(Request::is('teacher'))
+<li class="nav-item m-1 ml-2">
+    <button class="btn btn-primary text-white font-weight-normal">
+        <div class="row justify-content-left">
+            <img src="{{ asset('assets/icons/icon_hat.svg') }}" width="16px">
+            <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+            Курсы
+        </div>
+    </button>
+</li>
+@else
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_hat.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Курсы
+    </a>
+</li>
+@endif
+
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_courses.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Продукты
+    </a>
+</li>
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_lightning.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Задания
+    </a>
+</li>
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_users.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Пользователи
+    </a>
+</li>
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_file.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Файлы
+    </a>
+</li>
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_gamepad.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Геймификация
+    </a>
+</li>
+
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_cogwheel.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Настройки
+    </a>
+</li>
+
+<li class="nav-item  m-1 ml-2">
+    <a href="#" class="nav-link text-white font-weight-normal " >
+        <img src="{{ asset('assets/icons/icon_help.svg') }}"  width="16px">
+        <i class="fa fa-th-large mr-3 text-primary fa-fw"></i>
+        Помощь
+    </a>
+</li>
