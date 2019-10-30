7c027c89767a875123a0c250fe98932a94d302b7
jebbyk <zaicev9797@gmail.com>
2019-10-31 01:02:51 +0600

    [FRONT] sidebar buttons animation, teacher>courses imporove, course>create view

--- resources/views/teacher/course/partials/form.blade.php
+++ resources/views/teacher/course/partials/form.blade.php
@@ -1,37 +1,64 @@
-<div class="card-body">
-    <div class="form-group">
-        <label for="title">@lang('content.headline')</label>
-        <input type="text" class="form-control" name="title" maxlength="35"
-               value="{{$course->title ?? ""}}" required>
+<form class="form m-0 p-0" action="{{route('course.store')}}" method="post" enctype="multipart/form-data">
+    <input type="hidden" name="_token" value="{{ csrf_token() }}">
+    <div class="card-body" style="min-width:548px; max-width: 548px">
+
+        <div class="form-group">
+            <label for="title">@lang('content.headline')</label>
+            <input type="text" class="form-control" name="title" maxlength="35"
+                   placeholder="Введите название курса"
+                   value="{{$course->title ?? ""}}" required>
+        </div>
+        <div class="form-group">
+            <label for="slug">@lang('content.slug')</label>
+            <input class="form-control" type="text" name="slug"
+                   value="{{$course->slug ?? ""}}" readonly="">
+        </div>
+        <div class="form-group">
+            <label for="description_short">@lang('content.shortdescription')</label>
+            <textarea class="form-control" id="description_short" maxlength="300"
+                      style="height:128px; resize: none"
+                      placeholder="До 320 символов"
+                      name="description_short" required>{{$course->description_short ?? ""}}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="description">@lang('content.fulldescription')</label>
+            <textarea class="form-control" id="description" name="description"
+                      placeholder="До 1280 символов"
+                      style="height:128px; resize: none"
+                      required>{{$course->description ?? ""}}</textarea>
+        </div>
+        <div class="form-group">
+            <label for="tags">@lang('content.tagsns')</label>
+            <input type="text" class="form-control" name="tags"
+                   placeholder="До 10 тегов"
+                   value="{{$course->tags ?? ""}}">
+        </div>
+        <div class="form-group">
+            <label for="video">@lang('content.linktovideo')</label>
+            <input type="text" class="form-control" id="video" maxlength="300"
+                   placeholder="Вставьте ссылка на видео youtube"
+                      name="video">{{$course->video ?? ""}}</input>
+        </div>
+        <div class="form-group">
+            <label for="cost">@lang('content.setcrsprice')</label>
+            <input class="form-control" type="number" id="cost" max="100"
+                   placeholder="Введите стоимость курса"
+                   name="cost" min="0" required value="{{$course->cost ?? ""}}">
+        </div>
+
+        <p class="text-dark font-weight-normal" style="font-size:13px; line-height: 20px">Изображение курса</p>
+        <input type="text" class="form-control font-weight-bold text-dark" style="font-size:13px; line-height: 16px" placeholder="Ссылка на изображение курса">
+        <div class="card p-3 my-3" style="border-style: dashed">
+            <div class="row justify-content-center pt-3">
+                <img class="mr-2" style="opacity: 0.3" src="{{ asset('assets/icons/icon_downloadcloud.svg') }}" width="22px">
+                <p class="text-muted col-md-12 text-center">Загрузите изображения курса</p>
+            </div>
+        </div>
+
+        <div class="row justify-content-left mx-0">
+            <button class="btn btn-primary mr-1"  type="submit">Создать курс</button>
+            <button class="btn btn-light">Сбросить</button>
+        </div>
+
     </div>
-    <div class="form-group">
-        <label for="slug">@lang('content.slug')</label>
-        <input class="form-control" type="text" name="slug"
-               value="{{$course->slug ?? ""}}" readonly="">
-    </div>
-    <div class="form-group">
-        <label for="description_short">@lang('content.shortdescription')</label>
-        <textarea class="form-control" id="description_short" maxlength="300"
-                  name="description_short" required>{{$course->description_short ?? ""}}</textarea>
-    </div>
-    <div class="form-group">
-        <label for="description">@lang('content.fulldescription')</label>
-        <textarea class="form-control" id="description" name="description"
-                  required>{{$course->description ?? ""}}</textarea>
-    </div>
-    <div class="form-group">
-        <label for="tags">@lang('content.tagsns')</label>
-        <input type="text" class="form-control" name="tags"
-               value="{{$course->tags ?? ""}}">
-    </div>
-    <div class="form-group">
-        <label for="video">@lang('content.linktovideo')</label>
-        <textarea class="form-control" id="video" maxlength="300"
-                  name="video">{{$course->video ?? ""}}</textarea>
-    </div>
-    <div class="form-group">
-        <label for="cost">@lang('content.setcrsprice')</label>
-        <input class="form-control" type="number" id="cost" max="100"
-               name="cost" min="0" required value="{{$course->cost ?? ""}}">
-    </div>
-</div>
+</form>
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -102,45 +102,47 @@
             </p>
         </div>
 
-        <div class="row m-3">
+        <div class="row m-3" style="height:100vh">
 
-            <div class="col-md-3 m-0 my-2">
-                <div class="card" style=" height:324px; font-size:12px; border-style: dashed; border-width:1px; border-radius:4px border-color:rgba(0,0,0,0.1); background-color:#fbfbfb">
+            <div class="m-2 p-0" style="min-width: 280px; max-width: 280px">
+                <div class="card" style=" height:324px; font-size:12px; border-style: dashed; border-width:1px; border-radius:4px; border-color:rgba(0,0,0,0.1); background-color:#fbfbfb">
                     <div class="card-body  row m-0 justify-content-center text-muted">
-                        <div class="card-title text-dark" style="font-size:16px">
+                        <div class="card-title text-dark col-md-12" style="font-size:16px">
                             Новый курс
                         </div>
                         Добавте черновик нового курса. В любой момент вы его сможете наполнить уроками.
-                        <a href="{{ route('course.create') }}" class="btn btn-primary mb-0" style="font-size:13px">
-                            Создать тренинг
-                        </a>
+                        <form class="m-0 p-0" action="{{ route('course.create') }}">
+                            <button type="submit" class="btn btn-primary mb-0 col-md-12" style="font-size:13px">
+                                Создать курс
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
 
             @foreach($courses as $course)
-            <div class="col-md-3 m-0 my-2">
+            <div class="m-2" style="min-width: 280px; max-width: 280px">
 
                 <div class="card" style="height:324px; border-radius:4px">
-                    <div class="card-header m-0 set-bg" style="height:160px" data-setbg="{{config('static.static')}}/img/bg.jpg">
+                    <div class="card-header m-0 set-bg" style="height:160px; border-top-left-radius: 4px; border-top-right-radius: 4px" data-setbg="{{config('static.static')}}/img/bg.jpg">
                         <div class="row justify-content-end">
-                            <a href="#" class="p-4" id="#" data-toggle="dropdown"  aria-haspopup="ture" aria-expanded="false">
-                                <img src="{{ asset('assets/icons/icon_threedots.svg') }}" style="opacity: 0.3;" class="pb-1" width="4px">
+                            <a href="#" class="btn btn-icon" id="#" data-toggle="dropdown"  aria-haspopup="ture" aria-expanded="false">
+                                <img src="{{ asset('assets/icons/icon_threedots.svg') }}"  width="4px">
                             </a>
                             <div class="dropdown">
-                                <div class="dropdown-menu " style="font-size:13px; border: 0; box-shadow: 0px 2px 4px rgba(0,0,0,0.1); margin-left:-145px; margin-top: -50px" aria-labelledby="courseSettingsDropdown">
+                                <div class="dropdown-menu p-0" style="font-size:13px; border: 0; box-shadow: 0px 2px 4px rgba(0,0,0,0.1); margin-left:-512px; margin-top: -32px" aria-labelledby="courseSettingsDropdown">
                                     <a href="#" class="dropdown-item">Информация о курсе</a>
                                     <a href="#" class="dropdown-item">Содержимое курса</a>
                                     <a href="#" class="dropdown-item">Студенты</a>
                                     <a href="#" class="dropdown-item">Доступы</a>
                                     <a href="#" class="dropdown-item">Статистика</a>
-                                    <hr>
-                                    <form class="form-group col-md-12 px-0 d-flex-row"
+                                    <hr class="m-0">
+                                    <form class="form-group col-md-12 px-0 m-0 d-flex-row"
                                         onsubmit="if(confirm('@lang('content.reallydel')?')){return true}else{return false}"
                                         action="{{ route('course.destroy', [$course->id]) }}" method="post">
                                         <input type="hidden" name="_method" value="Delete">
                                         {{ csrf_field() }}
-                                        <button type="submit" style="bacground-color:rgba(0,0,0,0); color:red" class="dropdown-item">Удалить курс</a>
+                                        <button type="submit" style="background-color:rgba(0,0,0,0); color:red" class="dropdown-item">Удалить курс</button>
                                     </form>
                                 </div>
                             </div>
